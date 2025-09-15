dd64c8efd0109d8a9ff09b976c9b44efef990d87
iskander <iskandergafaroff@gmail.com>
2025-09-15 10:53:55 +0400

    refactoring

--- payment-service-app/src/main/java/ru/iprody/controller/PaymentController.java
+++ payment-service-app/src/main/java/ru/iprody/controller/PaymentController.java
@@ -1,12 +1,13 @@
 package ru.iprody.controller;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.iprody.model.Payment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,28 +15,23 @@ import java.util.Map;
 @RequestMapping("/payments")
 public class PaymentController {
 
-    private final Map<Long, Payment> payments;
+    private static final Map<Long, Payment> payments = new HashMap<>();
 
-    public PaymentController(Map<Long, Payment> payments) {
-        this.payments = payments;
-    }
-
-    @GetMapping
-    public List<Payment> getPayments() {
-        return payments.values().stream().toList();
-    }
-
-    @GetMapping("{paymentId}")
-    public Payment getPayment(@PathVariable Long paymentId) {
-        return payments.get(paymentId);
-    }
-
-    @PostConstruct
-    private void initializePayments() {
+    static {
         payments.put(1L, new Payment(1, 1.1));
         payments.put(2L, new Payment(2, 2.2));
         payments.put(3L, new Payment(3, 3.3));
         payments.put(4L, new Payment(4, 4.4));
         payments.put(5L, new Payment(5, 5.5));
+    }
+
+    @GetMapping
+    public List<Payment> getPayments() {
+        return new ArrayList<>(payments.values());
+    }
+
+    @GetMapping("{paymentId}")
+    public Payment getPayment(@PathVariable Long paymentId) {
+        return payments.get(paymentId);
     }
 }
