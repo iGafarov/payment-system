e57dd6fa52b8dc50415a70875ce80795db2dc6d5
iskander <iskandergafaroff@gmail.com>
2025-09-17 00:50:37 +0400

    added check error

--- payment-service-app/src/main/java/ru/iprody/PaymentServiceApplication.java
+++ payment-service-app/src/main/java/ru/iprody/PaymentServiceApplication.java
@@ -3,6 +3,8 @@ package ru.iprody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class PaymentServiceApplication {
 
