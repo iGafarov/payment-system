5121d0e26aaad8311a059b692e56c45be842f501
iskander <iskandergafaroff@gmail.com>
2025-09-17 00:51:43 +0400

    added compilation error

--- payment-service-app/src/main/java/ru/iprody/PaymentServiceApplication.java
+++ payment-service-app/src/main/java/ru/iprody/PaymentServiceApplication.java
@@ -3,12 +3,10 @@ package ru.iprody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 @SpringBootApplication
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentServiceApplication.class, args);
+        SpringApplication.run(PaymentServiceApplication.class, arg);
     }
 }
